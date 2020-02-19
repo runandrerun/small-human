a5e0da63e85235cd57574a7d52a4707f16b64256
andre.santiago <andre.santiago@area23hc.com>
2020-02-19 12:02:53 -0500

    Updates

--- precache-manifest.9088ce051660ed7264a3b1612032c523.js
+++ precache-manifest.9088ce051660ed7264a3b1612032c523.js
@@ -1,15 +1,15 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "cdeaccf6b4130b254f0ecf7ff865b64f",
+    "revision": "13872c6cf2bd791f06de6ae242e4a9a5",
     "url": "/small-human/index.html"
   },
   {
-    "revision": "c3ce1e3677f840afbbed",
-    "url": "/small-human/static/css/3.14d7b73b.chunk.css"
+    "revision": "f4e507efd0c18e085233",
+    "url": "/small-human/static/css/3.65f57460.chunk.css"
   },
   {
-    "revision": "9eded5c303aa6c1e1907",
-    "url": "/small-human/static/css/main.24f6485b.chunk.css"
+    "revision": "310f28939f7a084b481c",
+    "url": "/small-human/static/css/main.1724e4b9.chunk.css"
   },
   {
     "revision": "4c25469b0102d8b2d818",
@@ -20,16 +20,16 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/small-human/static/js/2.38304bf6.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "c3ce1e3677f840afbbed",
+    "revision": "f4e507efd0c18e085233",
     "url": "/small-human/static/js/3.e53c870f.chunk.js"
   },
   {
-    "revision": "9eded5c303aa6c1e1907",
+    "revision": "310f28939f7a084b481c",
     "url": "/small-human/static/js/main.a40bedb7.chunk.js"
   },
   {
-    "revision": "8a6e550a9670deca5ef5",
-    "url": "/small-human/static/js/runtime-main.1b683562.js"
+    "revision": "d454a4df0b50e289d0e2",
+    "url": "/small-human/static/js/runtime-main.148459ad.js"
   },
   {
     "revision": "40a4d27900d36bb545ec62f11202be05",
