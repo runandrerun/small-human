34b9aa3e7be41c32edba3323cd273e9f92c3dd0d
andre.santiago <andre.santiago@area23hc.com>
2020-02-20 20:12:04 -0500

    Recompiled latest Sass abstractions and restructures. Compiled new mobile menu.

---
+++ build/precache-manifest.df67ca46e7c458bd0d5a4a9df8917fd3.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc48c2b2d919ccf5a3eaf76e7c854416",
+    "url": "/small-human/index.html"
+  },
+  {
+    "revision": "0c36e6088578a4d44eb7",
+    "url": "/small-human/static/css/3.69706c9b.chunk.css"
+  },
+  {
+    "revision": "c573e06de0a5c6a4a9b7",
+    "url": "/small-human/static/css/main.ae3f6276.chunk.css"
+  },
+  {
+    "revision": "4e45fb10158d0975da62",
+    "url": "/small-human/static/js/2.8503b242.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/small-human/static/js/2.8503b242.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c36e6088578a4d44eb7",
+    "url": "/small-human/static/js/3.f982b21f.chunk.js"
+  },
+  {
+    "revision": "c573e06de0a5c6a4a9b7",
+    "url": "/small-human/static/js/main.52723363.chunk.js"
+  },
+  {
+    "revision": "2558787f12e943983211",
+    "url": "/small-human/static/js/runtime-main.ad4a682b.js"
+  },
+  {
+    "revision": "40a4d27900d36bb545ec62f11202be05",
+    "url": "/small-human/static/media/leaves.40a4d279.svg"
+  }
+]);
